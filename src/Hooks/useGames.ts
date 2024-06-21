e8cdd453adf28d1ed531cffe7b3f310cc0e01fd7
Austin Kinnard <austink145@gmail.com>
2024-06-21 16:41:57 -0400

    ok

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -32,7 +32,6 @@ export interface Game {
     name: string;
     background_image: string;
     parent_platforms: {platform:Platform}[];// The API is weird and returns an array of objects with a platform property
-    parent_platforms: {platform:Platform}[];// The API is weird and returns an array of objects with a platform property
   }
   
   interface FetchGamesResponse {
