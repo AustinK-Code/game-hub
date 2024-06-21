8fd6b506af39bf44fc2423b7070459e1b15b65d1
Austin Kinnard <austink145@gmail.com>
2024-06-21 16:55:52 -0400

    display platform Icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,18 +1,45 @@
-
-import { Platform } from '../Hooks/useGames'
-import { Text } from '@chakra-ui/react'
+import {
+  FaWindows,
+  FaXbox,
+  FaPlaystation,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../Hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
 interface Props {
-    platforms: {platform:Platform}[];
+  platforms: { platform: Platform }[];
 }
 
-const PlatformIconList = ({platforms}:Props) => {
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    linux: FaLinux,
+    apple: FaApple,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
 
   return (
-      <>
-      {platforms.map( ({platform}) => <Text key={platform.id}>{platform.name}</Text>)}
-      </>
-  )
-}
+    <>
 
-export default PlatformIconList
+      <HStack marginY={1}>    {/* puts in a nice line. Multiple of theme.space */}
+        {platforms.map(({ platform }) => (
+          <Icon as={iconMap[platform.slug]} key={platform.id} color='gray.500'/>
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
